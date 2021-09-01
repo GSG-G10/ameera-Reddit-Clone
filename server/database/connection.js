bcd4ccaaab80525744f427e5b8acf509603805b8
ameera abedalnaser <ameeraabed92@gmail.com>
2021-09-01 22:17:40 +0300

    connect data base with server and built sql Related #5

---
+++ server/database/connection.js
@@ -0,0 +1,26 @@
+const { Pool } = require("pg");
+require("env2")("./.env");
+const { NODE_ENV, URL_DB, DATABASE_URL, TEST_DB } = process.env;
+
+let UrlDb = DATABASE_URL;
+// switch (NODE_ENV) {
+//     case 'development':
+//         UrlDb = URL_DB;
+//         break;
+//     case 'production':
+//         UrlDb = DATABASE_URL;
+//         break;
+//     case 'test':
+//         UrlDb = TEST_DB;
+//         break;
+//     default:
+//         throw new Error('There\'s no environment');
+// }
+const option = {
+    connectionString: UrlDb,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+}
+
+module.exports = new Pool(option);
